ae967b61ea6648384c21032b357c7a8116ac46f8
mick roper <mick@theropers.co.uk>
2023-03-08 07:31:32 +0000

    wiring up commands

--- logging/new.go
+++ logging/new.go
@@ -1,11 +1,36 @@
 package logging
 
-import "go.uber.org/zap"
+import (
+	"encoding/json"
+	"fmt"
+	"strings"
 
-func New() *zap.Logger {
-	cfg := zap.NewProductionConfig()
-	cfg.Encoding = "console"
-	cfg.Level.SetLevel(zap.InfoLevel)
+	"go.uber.org/zap"
+)
+
+func New(level string) *zap.Logger {
+	rawJSON := fmt.Sprintf(`{
+	  "level": "%s",
+	  "encoding": "console",
+	  "outputPaths": ["stdout"],
+	  "errorOutputPaths": ["stderr"],
+	  "encoderConfig": {
+	    "messageKey": "message",
+	    "levelKey": "level",
+	    "levelEncoder": "lowercase"
+	  }
+	}`, level)
+
+	var cfg zap.Config
+	if err := json.Unmarshal([]byte(rawJSON), &cfg); err != nil {
+		panic(err)
+	}
+
+	if strings.EqualFold(level, "debug") {
+		cfg.EncoderConfig.StacktraceKey = "stack-trace"
+	} else {
+		cfg.EncoderConfig.StacktraceKey = ""
+	}
 
 	return zap.Must(cfg.Build())
 }
